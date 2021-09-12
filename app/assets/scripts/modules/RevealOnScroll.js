6edb407eafb5d79367d0cbdad9624e0e5ed29ab3
devcon <devcon95@gmail.com>
2021-09-13 03:06:10 +1000

    15.3 Revealing Elements on Scroll (Part 2)

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,30 +1,44 @@
+import throttle from "lodash/throttle";
+
 class RevealOnScroll {
   constructor() {
     this.itemsToReveal = document.querySelectorAll(".feature-item");
     this.hideInitially();
+    this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
     this.events();
   }
 
   events() {
-    window.addEventListener("scroll", () => {
-      this.itemsToReveal.forEach((item) => {
-        this.calculateIfScrolledTo(item);
-      });
+    window.addEventListener("scroll", this.scrollThrottle);
+  }
+
+  calcCaller() {
+    // console.log("Scroll function ran");
+    this.itemsToReveal.forEach((element) => {
+      if (element.isRevealed == false) {
+        this.calculateIfScrolledTo(element);
+      }
     });
   }
 
   calculateIfScrolledTo(element) {
-    let scrollPercent =
-      (element.getBoundingClientRect().y / window.innerHeight) * 100;
+    // console.log("Element was calcualted");
+    let scrollPercent = (element.getBoundingClientRect().y / window.innerHeight) * 100;
     if (scrollPercent < 75) {
       element.classList.add("reveal-item--is-visible");
+      element.isRevealed = true;
+      if (element.isLastItem) {
+        window.removeEventListener("scroll", this.scrollThrottle);
+      }
     }
   }
 
   hideInitially() {
-    this.itemsToReveal.forEach((item) => {
-      item.classList.add("reveal-item");
+    this.itemsToReveal.forEach((element) => {
+      element.classList.add("reveal-item");
+      element.isRevealed = false;
     });
+    this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true;
   }
 }
 
